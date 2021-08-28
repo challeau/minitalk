2440b38c88271f5730e2c74fd86734fa76ec625a
Chlotte <charlottealleaume99@gmail.com>
2021-08-29 00:36:18 +0200

    programs successfully talk

--- srcs/client.c
+++ srcs/client.c
@@ -2,24 +2,93 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include "../inc/libft/libft.h"
 
 static void	error(char *err_str)
 {
 	printf("[CLIENT] encountered an error!\n\tIssue: ");
 	printf("%s.\n", err_str);
-	perror("perror says: ");
+	perror("perror says");
 	exit(EXIT_FAILURE);
 }
 
-int	main(__attribute__((unused))int ac, __attribute__((unused))char **av)
-{
-	pid_t	pid;
+/* static void s_str(char *str) */
+/* { */
+/* 	int	i; */
+/* 	char	tmp; */
 
-	if (ac != 2)
+/* 	while (*str) */
+/* 	{ */
+/* 		i = 7; */
+/* 		tmp = *str; */
+/* 		while (i >= 0) */
+/* 		{ */
+/* 			if (((tmp >> i) & 0x01) == 0) */
+/* 				printf("0"); */
+/* 			else */
+/* 				printf("1"); */
+/* 			i--; */
+/* 		} */
+/* 		printf("\n"); */
+/* 		str++; */
+/* 	} */
+/* } */
+
+static void	send_string(char *str, pid_t pid)
+{
+	int	i;
+	char	tmp;
+
+	while (*str)
+	{
+		tmp = *str;
+		i = 7;
+		while (i >= 0)
+		{
+			if (((tmp >> i) & 0x01) == 0)
+			{
+				int ret = kill(pid, SIGUSR1);
+				if (ret < 0)
+					error("couldn't send signal");
+				//			printf("SIGUSR1 sent\n");
+			}
+			else
+			{
+				int ret = kill(pid, SIGUSR2);
+				if (ret < 0)
+					error("couldn't send signal");
+				//			printf("SIGUSR2 sent\n");
+			}
+			i--;
+			usleep(100);
+		}
+		str++;
+	}
+	for (int i = 0 ; i <= 8 ; i++)
+		if (kill(pid, SIGUSR1) < 0)
+			error("couldn't send signal");
+}
+
+/* static void lol(int sig) */
+/* { */
+/* 	printf("she's done\n"); */
+/* 	if (sig == SIGTERM) */
+/* 		exit(0); */
+/* } */
+
+int	main(int ac, char **av)
+{
+	pid_t	server_pid;
+
+	if (ac != 3)
 		error("wrong  number of arguments");
-	pid = atoi(av[1]);
-	if (kill(pid, 0) == -1)
+	server_pid = ft_atoi(av[1]);
+	if (kill(server_pid, 0) == -1)
 		error("wrong pid or permissions");
-	kill(pid, SIGUSR1);
+	send_string(av[2], server_pid);
+	/* signal(SIGUSR1, lol); */
+	/* signal(SIGTERM, lol); */
+	while (420)
+		sleep(69);
 	return (0);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -4,42 +4,80 @@
 #include <stdlib.h>
 #include "../inc/libft/libft.h"
 
-static void	clean_exit(void)
-{
-	printf("[SERVER] exiting...\n");
-	exit(EXIT_SUCCESS);
-}
+/* printf("%c%c%c%c%c%c%c%c\n",  (c & 0x80 ? '1' : '0'), */
+/*        (c & 0x40 ? '1' : '0'), */
+/*        (c & 0x20 ? '1' : '0'), */
+/*        (c & 0x10 ? '1' : '0'), */
+/*        (c & 0x08 ? '1' : '0'), */
+/*        (c & 0x04 ? '1' : '0'), */
+/*        (c & 0x02 ? '1' : '0'), */
+/*        (c & 0x01 ? '1' : '0') ); */
 
-static void	signal_handler(int sig)
+static void	signal_handler(int sig, siginfo_t *info,
+			       __attribute__((unused))void *context)
 {
-	static int		i;
-	struct sigaction	siga;
+	static uint8_t		i = 0;
+	static char		c = 0;
+	static char		*str = 0;
+	static int		pid = 0;
 
+//	printf("entering handler\n");
+	str = ft_strdup("");
+	if (info->si_pid)
+		pid = info->si_pid;
 	if (sig == SIGUSR1)
 	{
-		printf("[SERVER] received signals from %d client.s\n", i);
+//		printf("received SIGUSR1\n");
+		c = c << 1;
 		i++;
 	}
-	if (sig == SIGINT)
-		clean_exit();
-	siga.sa_handler = &signal_handler;
-	siga.sa_flags = SA_NODEFER;
-	sigaction(SIGUSR1, &siga, NULL);
-	sigaction(SIGINT, &siga, NULL);
-	pause();
+	if (sig == SIGUSR2)
+	{
+//		printf("received SIGUSR2\n");
+		c = c << 1;
+		c |= 0x01;
+		i++;
+		/* printf("%c%c%c%c%c%c%c%c\n",  (c & 0x80 ? '1' : '0'), */
+		/*        (c & 0x40 ? '1' : '0'), */
+		/*        (c & 0x20 ? '1' : '0'), */
+		/*        (c & 0x10 ? '1' : '0'), */
+		/*        (c & 0x08 ? '1' : '0'), */
+		/*        (c & 0x04 ? '1' : '0'), */
+		/*        (c & 0x02 ? '1' : '0'), */
+		/*        (c & 0x01 ? '1' : '0') ); */
+	}
+	if (i == 8)
+	{
+		str = ft_add_char(str, c);
+		ft_putstr_fd(1, str);
+		ft_putstr_fd(1, "\n");
+		i = 0;
+		c = 0;
+	}
+	/* if (kill(pid, SIGUSR1) == -1) */
+	/* 	perror("yikes"); */
 }
 
 int	main(void)
 {
 	pid_t			pid;
 	struct sigaction	sig;
+	sigset_t		block_mask;
 
 	pid = getpid();
 	printf("[SERVER] pid: %d\n", pid);
-	sig.sa_handler = &signal_handler;
-	sig.sa_flags = SA_NODEFER;
+	
+	sig.sa_handler = NULL;
+	sig.sa_sigaction = &signal_handler;
+	sig.sa_flags = SA_SIGINFO;
+
+	sigemptyset(&block_mask);
+	sigaddset(&block_mask, SIGINT);
+	sig.sa_mask = block_mask;
+
 	sigaction(SIGUSR1, &sig, NULL);
-	sigaction(SIGINT, &sig, NULL);
-	pause();
+	sigaction(SIGUSR2, &sig, NULL);
+	while (420)
+		sleep(69);
 	return (0);
 }
