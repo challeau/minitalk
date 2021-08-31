3c0363151a57ebac21e8b01ae57a5c6301188b02
Chlotte <charlottealleaume99@gmail.com>
2021-08-31 23:53:49 +0200

    bit of norm

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -1,4 +1,16 @@
-# ifndef MINITALK_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inc/minitalk.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: challeau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/31 23:52:24 by challeau          #+#    #+#             */
+/*   Updated: 2021/08/31 23:52:24 by challeau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
 # define MINITALK_H
 # include <stdio.h>
 # include <unistd.h>
@@ -11,4 +23,4 @@ void	kill_handler(__attribute__((unused))int sig);
 void	reset(char **str, uint8_t *bit, uint8_t *byte);
 void	print_pid(pid_t pid);
 
-# endif
+#endif
